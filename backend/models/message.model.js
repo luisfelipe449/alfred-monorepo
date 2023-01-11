6d4d6671cab393ff180b350338c32374197e7bcf
Luis <1luisfelipe2@gmail.com>
2023-01-10 22:14:53 -0400

    muda apontamentos do banco

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -2,8 +2,19 @@ import { Sequelize, DataTypes } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
+// const sequelize = new Sequelize(
+//   `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`
+// );
 const sequelize = new Sequelize(
-  `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+    timestamps: false,
+  }
 );
 
 export const Messages = sequelize.define(
