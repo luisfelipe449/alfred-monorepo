890dc2607d43c8153091c773adfb89aca0ac3f6c
Luis <1luisfelipe2@gmail.com>
2023-01-10 22:26:21 -0400

    muda connection strings

--- backend/models/clientePedido.model.js
+++ backend/models/clientePedido.model.js
@@ -10,10 +10,17 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
     dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    dialectOptions: {
+      socketPath: process.env.INSTANCE_UNIX_SOCKET,
+    },
     logging: false,
-    timestamps: false,
   }
 );
 
